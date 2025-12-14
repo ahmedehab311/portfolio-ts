90e8fdfd86582942cd1e9c5905cdb38a5025b653
ahmedehab311 <ahmedehab.aa47@gmail.com>
2025-12-14 21:32:07 +0200

    up

--- app/components/heroSection/myDescrption.tsx
+++ app/components/heroSection/myDescrption.tsx
@@ -11,9 +11,9 @@ export default function MyDescrption({ colors }: TDescrption) {
     return (
         <>
             <h1 className={`text-4xl md:text-5xl lg:text-6xl font-bold ${colors.textPrimary} mb-6`}>
-                FullStack <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-cyan-400">Developer</span>
+                Frontend <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-cyan-400">Developer</span>
             </h1>
-
+            
             {isLoading ? (
                 <div className="space-y-1 max-w-2xl mb-8 ">
                     <div className="h-6 bg-gray-300 dark:bg-gray-700 rounded w-3/4 animate-pulse"></div>
