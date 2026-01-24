62d1d29d84eb1b689eddd1ceabf7482482f35fe2
ahmedehab311 <ahmedehab.aa47@gmail.com>
2026-01-24 14:43:13 +0200

    update project section

--- app/components/projects/ProjectsSection.client.tsx
+++ app/components/projects/ProjectsSection.client.tsx
@@ -119,8 +119,7 @@ export default function ProjectsSection() {
             </div>
 
 
-            <motion.div
-
+            <div
                 className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
             >
                 <AnimatePresence mode="popLayout">
@@ -145,7 +144,7 @@ export default function ProjectsSection() {
                         ))
                     )}
                 </AnimatePresence>
-            </motion.div>
+            </div>
             <ProjectDetailsModal
                 projectId={selectedId}
                 project={selectedProject}
